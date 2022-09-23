b5a5829b91f4896c264c619127f77e703cf1f032
Mehdi Anhichem <m.anhichem@liverpool.ac.uk>
2022-09-23 12:52:35 +0100

    Adding notebook and updated version of MFDF_GPflow

--- MFDF_GPflow_light.py
+++ MFDF_GPflow_light.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Python Mulfidelity Surrogate model from various information sources using Gaussian Processes 
-based on GPflow.
+based on GPflow. Light version limited to standard application.
 
 Mehdi Anhichem
 University of Liverpool
@@ -28,16 +28,18 @@ from gpflow.utilities import print_summary
 #==============================================================================
 
 def IntermediateSurrogate(X_training, Y_training, kernel, mean_function = None, name = 'information_source',
-                          SVGP = False, M_training = 50, train_Z = True, method_Z_init = 'random', N_grid = 100,
-                          avail_IS = [],
-                          likelihood_variance = 1.0, train_likelihood_var = True,
-                          elbo_monitoring = False):
+                          likelihood_variance = 1e-3, train_likelihood_var = True):
     ''' Function that trains an intermediate surrogate model for an information
         source using Gaussian Processes. 
     '''
     ''' Inputs:
         X_training: list of D arrays with training data inputs (need to be reshaped to (N,D)).
         Y_training: array with training data output (need to be reshaped to (N,1)).
+        kernel: GPflow kernel function.
+        mean_function: GPflow mean function.
+        name: string with the name of the information source.
+        likelihood_variance: float initial value of likelihood variance.
+        train_likelihood_var: boolean on training likelihood variance.
     '''
     ''' Outputs:
         model: GPflow trained model.
@@ -51,52 +53,19 @@ def IntermediateSurrogate(X_training, Y_training, kernel, mean_function = None, 
     if all([len(X_training[k])==len(Y_training) for k in range(len(X_training))]):
         Y = Y_training.reshape(-1,1)
     ''' Train GP '''
-    if not SVGP:
-        # Choose a Kernel
-        # print_summary(kernel)
-        # Construct a model
-        model = gpflow.models.GPR(data = (X,Y), kernel = kernel, mean_function = mean_function)
-        print_summary(model)
-        # Set likelihood variance
-        model.likelihood.variance.assign(likelihood_variance)
-        gpflow.set_trainable(model.likelihood.variance, train_likelihood_var)
-        print_summary(model)
-        #model.likelihood.variance.assign(0.1) # Noise variance in the model
-        #model.kernel.lengthscales.assign(0.3)
-        # Optimize the model parameters
-        opt = gpflow.optimizers.Scipy()
-        opt_logs = opt.minimize(model.training_loss, model.trainable_variables, options = dict(maxiter=100))
-        print_summary(model)
-    if SVGP:
-        N = len(Y_training)
-        if method_Z_init == 'random':
-            # Random initialisation of the inducing points.
-            M = M_training
-            shuffled_range_Z = random.sample(range(X.shape[0]), M)
-            Z = X[shuffled_range_Z,:]
-        if method_Z_init == 'M_first':
-            # Initialisation of the inducing points to the M-th first points of X.
-            M = M_training
-            Z = X[:M, :].copy()
-        if method_Z_init == 'grid_2D':
-        # Uniform grid of inducing points 
-            Z = create_Z_2D(N_grid)
-            M = Z.shape[0]
-        if method_Z_init == 'grid_4D':
-            Z = create_Z_4D(avail_IS, N_grid)
-            M = Z.shape[0]
-        # whiten=True toggles the fₛ=Lu parameterization.
-        # whiten=False uses the original parameterization.
-        model = gpflow.models.SVGP(kernel, gpflow.likelihoods.Gaussian(), Z, num_data=N, whiten=True)
-        # Set likelihood variance
-        model.likelihood.variance.assign(likelihood_variance)
-        gpflow.set_trainable(model.likelihood.variance, train_likelihood_var)
-        gpflow.set_trainable(model.inducing_variable, train_Z) # Enable the model to train the inducing locations.
-        print_summary(model)
-        train_dataset = tf.data.Dataset.from_tensor_slices((X, Y)).repeat().shuffle(N)
-        maxiter = ci_niter(20000)
-        logf = run_adam(model, maxiter, train_dataset)
-        print_summary(model)
+    # Choose a Kernel
+    # print_summary(kernel)
+    # Construct a model
+    model = gpflow.models.GPR(data = (X,Y), kernel = kernel, mean_function = mean_function)
+    # Set likelihood variance
+    model.likelihood.variance.assign(likelihood_variance)
+    gpflow.set_trainable(model.likelihood.variance, train_likelihood_var)
+    print_summary(model)
+    # Optimize the model parameters
+    opt = gpflow.optimizers.Scipy()
+    opt_logs = opt.minimize(model.training_loss, model.trainable_variables, options = dict(maxiter=100))
+    print_summary(model)
+    
     return model
 
 
@@ -126,7 +95,7 @@ def FidelityStd(X_prediction, constant_fidelity):
         constant_fidelity: constant float value of the fidelity function over the prediction space. 
     '''
     ''' Outputs:
-        Y_fid_std: Tensor of the shape (N_prediction, 1) of the fidelity function.
+        Y_fid_std: tensor of the shape (N_prediction, 1) of the fidelity function.
     '''  
     Y_std_fid = constant_fidelity * np.ones((X_prediction.shape[0], 1))
     Y_std_fid = tf.convert_to_tensor(Y_std_fid)
@@ -141,8 +110,8 @@ def MultifidelityFusion(IntermediateSurrogates):
         prediction. The number of 3-arrays lists is the number of information sources.
     '''
     ''' Outputs:
-        Y_mean_multi: Tensor of the shape (N_prediction, 1) of the multifidelity mean.
-        Y_std_multi: Tensor of the shape (N_prediction, 1) of the multifidelity standard deviation.
+        Y_mean_multi: tensor of the shape (N_prediction, 1) of the multifidelity mean.
+        Y_std_multi: tensor of the shape (N_prediction, 1) of the multifidelity standard deviation.
     '''
     M = len(IntermediateSurrogates)
     ''' Construction of the total standard deviation '''
@@ -171,35 +140,6 @@ def MultifidelityFusion(IntermediateSurrogates):
 #==============================================================================
 # UTILITIES FUNCTIONS
 #==============================================================================
-
-def run_adam(model, iterations, train_dataset):
-    ''' Utility function running the Adam optimizer.
-    '''
-    ''' Inputs:
-        model: GPflow model.
-        interations: number of iterations.
-        train_dataset: Tensorflow Shuffle Repeat Dataset made of training data (X,Y).
-    '''
-    minibatch_size = 100
-    # Create an Adam Optimizer action
-    logf = []
-    logf_text = []
-    train_iter = iter(train_dataset.batch(minibatch_size))
-    training_loss = model.training_loss_closure(train_iter, compile=True)
-    optimizer = tf.optimizers.Adam()
-    @tf.function
-    def optimization_step():
-        optimizer.minimize(training_loss, model.trainable_variables)
-
-    for step in range(iterations):
-        optimization_step()
-        if step % 10 == 0:
-            elbo = -training_loss().numpy()
-            # print(step, elbo)
-            logf.append(elbo)
-            
-    return logf
-
 def dataset_separation(X_t, Y_t, ratio_training_validation):
     ''' Fonction used to split a given dataset into two dataset.
     '''
@@ -252,14 +192,17 @@ def PlotPredictionSpace2D(design_space):
         design_space: list of 2 2-elements lists made from the design interval for each dimension.
     '''
     ''' Outputs:
-        X_prediction: Array of the shape (N_prediction, 2) used for prediction.
-        X_plot1: Array formed as a grid of values of dimension 1 for 3D-plot.
-        X_plot2: Array formed as a grid of values of dimension 2 for 3D-plot.
+        X_prediction: array of the shape (N_prediction, 2) used for prediction.
+        X_plot1: array formed as a grid of values of dimension 1 for 3D-plot.
+        X_plot2: array formed as a grid of values of dimension 2 for 3D-plot.
     '''    
-
     D = len(design_space)
-    X_plot1, X_plot2 = np.mgrid[design_space[0][0]:design_space[0][1]:8, design_space[1][0]:design_space[1][1]:8]
-    X_prediction_plot = np.column_stack([[X_plot1, X_plot2]]).T
-    X_prediction_plot = X_prediction_plot.reshape(X_prediction_plot.shape[0]*X_prediction_plot.shape[1], 2)
-            
-    return X_prediction_plot, X_plot1, X_plot2
+    try:
+        X_plot1, X_plot2 = np.mgrid[design_space[0][0]:design_space[0][1]:8, design_space[1][0]:design_space[1][1]:8]
+        X_prediction_plot = np.column_stack([[X_plot1, X_plot2]]).T
+        X_prediction_plot = X_prediction_plot.reshape(X_prediction_plot.shape[0]*X_prediction_plot.shape[1], 2)
+        return X_prediction_plot, X_plot1, X_plot2
+    except D > 2:
+        return 'Dimesion of the design space is higher than 2'
+        
+    
